bfeeffbe493789703d23d0c3202a4e9dc7eace68
AlexMagar <lx.mgr07@gmail.com>
2023-10-25 21:56:41 +1100

    feat: debug

--- src/typeAssertion.ts
+++ src/typeAssertion.ts
@@ -24,7 +24,7 @@ let myVal: string = addOrConcat(2,2,'concat') as string
 (10 as unknown) as string
 
 //DOM
-const img = document.querySelector('img')!q
+const img = document.querySelector('img')!
 const myImg = document.getElementById('#img')! as HTMLImageElement
 
-img.src
+img.src 
