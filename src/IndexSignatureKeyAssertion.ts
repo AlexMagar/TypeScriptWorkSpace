262ee2c68b894ee62a468c43ee8415b6467022b2
AlexMagar <lx.mgr07@gmail.com>
2023-10-25 21:56:24 +1100

    feat: Index Signature and Key Assertion

---
+++ src/IndexSignatureKeyAssertion.ts
@@ -0,0 +1,16 @@
+// Index Signature
+
+interface TransactionObj{
+    Pizza: number,
+    Books: number,
+    Job: number,
+}
+
+const todayTransactions: TransactionObj = {
+    Pizza : -10,
+    Books : -5,
+    Job: 50
+}
+
+ let prop: string = 'Pizza'
+ 
