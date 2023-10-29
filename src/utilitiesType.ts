389491247cd02eb2403fc42d2c6c6efcfa0c44fc
AlexMagar <lx.mgr07@gmail.com>
2023-10-29 21:51:07 +1100

    feat: utilities type

--- src/utilitiesType.ts
+++ src/utilitiesType.ts
@@ -29,6 +29,52 @@ const recordAssignment = (assign: Required<Assignment>): Assignment => {
     return assign
 }
 
-const assignVerified: Readonly.<Assignment> = {
+const assignVerified: Readonly<Assignment> = {
     ...assignGraded, verified: true
 }
+
+recordAssignment({...assignGraded, verified: true})
+
+//Record
+// <string, string> =>1st string is key and follow by value
+const hexColorMap: Record<string, string> = {
+    red: "FF0000",
+    green: "00FF00",
+    blue: "0000FF"
+}
+
+type Students = "Lax" | "Kelly"
+type LetterGrades = "A" | "B" | "C" | "D" | "U"
+
+const finalGrades: Record<Students, LetterGrades> ={
+    Lax : "A",
+    Kelly: "B"
+}
+
+// pick and omit
+type AssignResult = Pick <Assignment, "studentId" | "grade">
+
+const score: AssignResult = {
+    studentId: "K123",
+    grade: 45
+}
+
+type AssignPreview = Omit<AssignResult, "grade" | "verified" >
+
+const preview: AssignPreview = {
+    studentId: "2345ED", 
+    title: "Final Grade",
+}
+
+// Exclude and Extract
+type adjuctGrade = Exclude<LetterGrades, "u">
+
+type highGrade = Extract<LetterGrades, "A"  | "B">
+
+
+// Return type
+
+type newAssign = { title: string, points: number}
+
+
+ 
